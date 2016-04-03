dd27786aadbf0bdad9ba6f124b92694961da0531
shigemk2 <i.am.shige@gmail.com>
2016-04-03 15:49:12 +0900

    require cssするの面倒だから直接link relすることにした

--- src/example.jsx
+++ src/example.jsx
@@ -3,7 +3,7 @@ var ReactDOM = require('react-dom');
 var DatePicker = require('react-datepicker');
 var moment = require('moment');
 
-require('react-datepicker/dist/react-datepicker.css');
+// require('react-datepicker/dist/react-datepicker.css');
 
 var HelloWorld = React.createClass({
     displayName: 'Example',
